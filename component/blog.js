e01d59485f4bc2d2e8c1eae5dff8115dde7d28a0
stallioninfotech238 <58873663+stallioninfotech238@users.noreply.github.com>
2022-07-08 16:34:12 +0530

    complete

--- component/blog.js
+++ component/blog.js
@@ -1,17 +1,35 @@
 import { useEffect, useState } from 'react';
-var Meta = require('html-metadata-parser');
+import styled from "styled-components";
+const CustomImg = styled.div`
+  height: 300px;
+  background-image: url(${({ src }) => src});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center center;
+`;
+export default function Blog(props) {
+  const [imgUrl, setImgUrl] = useState('');
 
-export default function Blog() {
-  useEffect(() => {
-    myFunction();
-  }, []);
+  useEffect(async () => {
+    if ((props.data.url ?? '') !== '') {
+      try {
+        const response = await fetch(`https://jsonlink.io/api/extract?url=${props.data.url}`);
+        var d = await response.json();
+        if ((d.images ?? []).length > 0) {
+          setImgUrl(d.images[0]);
+        }
+      } catch (error) {
+        return [];
+      }
+    }
+  }, [props]);
   return (
-        <div className='col-6'>1</div>
+    <div className='col-4'>
+      <div className='m-2 card' style={{cursor:'pointer'}} onClick={props.onTap}>
+        <CustomImg className='w-100' src={imgUrl} />
+        <h5 className='mx-2'>{props.data.title}</h5>
+        <h6 className='mx-2'>{props.data.text}</h6>
+      </div>
+    </div>
   )
-}
-function myFunction() {
-  Meta.parser('https://learnstartup.net/p/BJQWO5_Wnx', function (err, result) {
-
-    console.log(result);
-})
 }
--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,27 @@ import { gql, useQuery } from "@apollo/client";
 import client from "../apollo-client";
 import { useEffect, useState } from 'react';
 import Blog from "../component/blog";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 export default function Home() {
   const [article, setArticle] = useState([]);
   const [page, setPage] = useState(1);
-
-
-
+  const [hasMore, setHasMore] = useState(true);
   useEffect(() => {
     getData();
-  }, [page]);
+    // window.onscroll = function (ev) {
+    //   if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !isLoading) {
+    //     page = page + 1;
+    //     getData();
+    //   }
+    // };
+  }, []);
+  // useEffect(() => {
+  //   getData();
+  // }, [page]);
 
-  const getData = async() => {
-
+  const getData = async () => {
+    console.log(page);
     const { data } = await client.query({
       query: gql`
       query {
@@ -32,19 +41,25 @@ export default function Home() {
       }      
       `,
     });
-  console.log(data);
-    setArticle([...article,...data['retrievePageArticles']]);
+    setArticle((article) => [...article, ...data['retrievePageArticles']]);
+    setPage((page) => page + 1);
+    setHasMore(data['retrievePageArticles'].length === 30);
   }
   return (
     <div className="container">
-      <div className='my-2 row'>
-        <Blog/>
-        <Blog/>
-        <Blog/>
-        <Blog/>
-        <Blog/>
-        <Blog/>
+
+      <InfiniteScroll
+        dataLength={article.length}
+        next={getData}
+        hasMore={hasMore}
+        loader={<h3> Loading...</h3>}
+        endMessage={<h4>Nothing more to show</h4>}
+      > <div className='my-2 row'>
+          {article.map((data) => <Blog key={data.id} data={data} onTap={() => {
+            window.open(data.url ?? '', '_blank');
+          }} />)}
         </div>
+      </InfiniteScroll>
     </div>
   )
 }
